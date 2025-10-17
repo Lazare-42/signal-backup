bd3fb765d92c6faee724ccaa2c8daccf07bfd077
Lazare Rossillon <lazrossi@student.42.fr>
2025-10-17 12:13:32 +0200

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "Signal Desktop message importer for PostgreSQL";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        haskellPackages = pkgs.haskellPackages;
+
+        signal-importer = haskellPackages.callCabal2nix "signal-importer" ./. {
+          # Ensure we use system PostgreSQL library
+          postgresql-libpq = haskellPackages.postgresql-libpq.override {
+            postgresql = pkgs.postgresql_14;
+          };
+        };
+
+      in {
+        packages = {
+          default = signal-importer;
+          signal-importer = signal-importer;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Haskell toolchain
+            ghc
+            cabal-install
+            haskell-language-server
+
+            # PostgreSQL
+            postgresql_14
+
+            # SQLite
+            sqlite
+
+            # Development tools
+            zlib
+            pkg-config
+          ];
+
+          # Set up PostgreSQL library paths
+          shellHook = ''
+            export LD_LIBRARY_PATH="${pkgs.postgresql_14}/lib:$LD_LIBRARY_PATH"
+            export PKG_CONFIG_PATH="${pkgs.postgresql_14}/lib/pkgconfig:$PKG_CONFIG_PATH"
+            echo "🚀 Signal Importer development environment loaded"
+            echo "   - GHC: $(ghc --version)"
+            echo "   - Cabal: $(cabal --version | head -1)"
+            echo "   - PostgreSQL: $(pg_config --version)"
+            echo ""
+            echo "Quick start:"
+            echo "  cabal build          # Build the project"
+            echo "  cabal run signal-importer -- --help"
+            echo "  bd list              # View project issues"
+          '';
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${signal-importer}/bin/signal-importer";
+        };
+      }
+    );
+}
